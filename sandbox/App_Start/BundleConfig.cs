ae8aba627c537b7da9dfce80292e3125b9530435
kickno <sakiko.judge@gmail.com>
2015-01-18 13:29:29 -0500

    fixed non ascii char in a tag

--- sandbox/App_Start/BundleConfig.cs
+++ sandbox/App_Start/BundleConfig.cs
@@ -16,6 +16,7 @@ namespace sandbox
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.unobtrusive-ajax*",
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
